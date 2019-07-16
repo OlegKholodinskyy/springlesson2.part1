778225fb07ed8458bf26430f978b3e3d3f0d4549
Oleg_hos <ooleguner@gmail.com>
2019-07-16 16:41:04 +0300

    1

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -31,13 +31,6 @@ public class Test {
     @RequestMapping(method = RequestMethod.GET, value = "/test", produces = "texp/plain")
     public @ResponseBody
     String callByBean() {
-        service1.setId(11L);
-        service1.setName("nameService-1");
-        service1.setParamsToCall(Arrays.asList("Param-1.1", "Param-1.2", "Param-1.3"));
-
-        service2.setId(12L);
-        service2.setName("nameService-2");
-        service2.setParamsToCall(Arrays.asList("Param-2.1", "Param-2.2"));
 
         int size1 = service1.getParamsToCall().size();
         int size2 = service2.getParamsToCall().size();
@@ -51,20 +44,12 @@ public class Test {
             map2.put(i, service2.getParamsToCall().get(i));
         }
 
-
-        step1.setId(100L);
-        step1.setServiceFrom(service1);
-        step1.setServiceTo(service2);
         step1.setParamsServiceFrom(map1);
         step1.setParamsServiceTo(map2);
 
-        step2.setId(102L);
-        step2.setServiceFrom(service1);
-        step2.setServiceTo(service2);
         step2.setParamsServiceFrom(map1);
         step2.setParamsServiceTo(map2);
 
-        route.setId("1000");
         route.setSteps(Arrays.asList(step1, step2));
 
 
